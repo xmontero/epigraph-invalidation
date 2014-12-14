96a70dfda4529f3a1f677570de8bff437d9ed5e0
Xavi Montero <xmontero@dsitelecom.com>
2014-12-14 18:35:02 +0100

    Adds instantiation for several sizes in the front controller. Changes the controller to accept both key and size.

--- web/RandomIcon.php
+++ web/RandomIcon.php
@@ -13,8 +13,53 @@ class DefaultController
 {
 	public function run()
 	{
-		$ig = new \Xmontero\EpigraphInvalidation\IconGenerator( "abcd" );
-		$ig->output( 512 );
+		$key = $this->getKey();
+		$size = $this->getSize();
+		
+		$ig = new \Xmontero\EpigraphInvalidation\IconGenerator( $key );
+		$ig->output( $size );
+	}
+	
+	public function getKey()
+	{
+		$key = filter_input( INPUT_GET, 'key', FILTER_SANITIZE_ENCODED );
+		
+		if( is_null( $key ) )
+		{
+			throw new \InvalidArgumentException( 'The parameter "key" must be present and be a string.' );
+		}
+		else
+		{
+			if( $key == false )
+			{
+				throw new \InvalidArgumentException( 'The parameter "key" must be a string.' );
+			}
+		}
+		
+		return $key;
+	}
+	
+	public function getSize()
+	{
+		$minSize = 4;
+		$maxSize = 1024;
+		
+		$sizeOptions = array( 'options' => array( 'min_range' => $minSize, 'max_range' => $maxSize ) );
+		$size = filter_input( INPUT_GET, 'size', FILTER_VALIDATE_INT, $sizeOptions );
+		
+		if( is_null( $size ) )
+		{
+			throw new \InvalidArgumentException( 'The parameter "size" must be present and be an integer between ' . $minSize . ' and ' . $maxSize . '.' );
+		}
+		else
+		{
+			if( $size === false )
+			{
+				throw new \InvalidArgumentException( 'The parameter "size" must be an integer between ' . $minSize . ' and ' . $maxSize . '.' );
+			}
+		}
+		
+		return $size;
 	}
 }
 
