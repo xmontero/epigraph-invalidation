a0b8bba457867d212320722e440bad7c4a571fbe
Xavi Montero <xmontero@dsitelecom.com>
2014-12-16 02:00:06 +0100

    Adds getting straight and diagonal neighbours. Straight are directly set. Diagonal are calculated.

--- src/LayerDownDot.php
+++ src/LayerDownDot.php
@@ -4,6 +4,10 @@ namespace Xmontero\EpigraphInvalidation;
 class LayerDownDot
 {
 	private $filled;
+	private $leftNeighbour;
+	private $rightNeighbour;
+	private $topNeighbour;
+	private $bottomNeighbour;
 	
 	public function __construct()
 	{
@@ -18,5 +22,152 @@ class LayerDownDot
 	public function setFilled( $filled )
 	{
 		$this->filled = $filled;
+	}
+	
+	public function getLeftNeighbour()
+	{
+		return $this->leftNeighbour;
+	}
+	
+	public function setLeftNeighbour( LayerDownDot $leftNeighbour)
+	{
+		$this->leftNeighbour = $leftNeighbour;
+	}
+	
+	public function getRightNeighbour()
+	{
+		return $this->rightNeighbour;
+	}
+	
+	public function setRightNeighbour( LayerDownDot $rightNeighbour)
+	{
+		$this->rightNeighbour = $rightNeighbour;
+	}
+	
+	public function getTopNeighbour()
+	{
+		return $this->topNeighbour;
+	}
+	
+	public function setTopNeighbour( LayerDownDot $topNeighbour)
+	{
+		$this->topNeighbour = $topNeighbour;
+	}
+	
+	public function getBottomNeighbour()
+	{
+		return $this->bottomNeighbour;
+	}
+	
+	public function setBottomNeighbour( LayerDownDot $bottomNeighbour)
+	{
+		$this->bottomNeighbour = $bottomNeighbour;
+	}
+	
+	public function getTopLeftNeighbour()
+	{
+		$topLeft = $this->getCoherentStraightNeighbours( $this->getTopNeighbour(), $this->getLeftNeighbour() );
+		
+		if( is_null( $topLeft ) )
+		{
+			$result = null;
+		}
+		else
+		{
+			$top = $topLeft[ 'dot1' ];
+			$left = $topLeft[ 'dot2' ];
+			
+			$result = $this->getSameDiagonalNeighbour( $top->getLeftNeighbour(), $left->getTopNeighbour() );
+		}
+		
+		return $result;
+	}
+	
+	public function getTopRightNeighbour()
+	{
+		$topRight = $this->getCoherentStraightNeighbours( $this->getTopNeighbour(), $this->getRightNeighbour() );
+		
+		if( is_null( $topRight ) )
+		{
+			$result = null;
+		}
+		else
+		{
+			$top = $topRight[ 'dot1' ];
+			$right = $topRight[ 'dot2' ];
+			
+			$result = $this->getSameDiagonalNeighbour( $top->getRightNeighbour(), $right->getTopNeighbour() );
+		}
+		
+		return $result;
+	}
+	
+	public function getBottomLeftNeighbour()
+	{
+		$bottomLeft = $this->getCoherentStraightNeighbours( $this->getBottomNeighbour(), $this->getLeftNeighbour() );
+		
+		if( is_null( $bottomLeft ) )
+		{
+			$result = null;
+		}
+		else
+		{
+			$bottom = $bottomLeft[ 'dot1' ];
+			$left = $bottomLeft[ 'dot2' ];
+			
+			$result = $this->getSameDiagonalNeighbour( $bottom->getLeftNeighbour(), $left->getBottomNeighbour() );
+		}
+		
+		return $result;
+	}
+	
+	public function getBottomRightNeighbour()
+	{
+		$bottomRight = $this->getCoherentStraightNeighbours( $this->getBottomNeighbour(), $this->getRightNeighbour() );
+		
+		if( is_null( $bottomRight ) )
+		{
+			$result = null;
+		}
+		else
+		{
+			$bottom = $bottomRight[ 'dot1' ];
+			$right = $bottomRight[ 'dot2' ];
+			
+			$result = $this->getSameDiagonalNeighbour( $bottom->getRightNeighbour(), $right->getBottomNeighbour() );
+		}
+		
+		return $result;
+	}
+	
+	private function getCoherentStraightNeighbours( LayerDownDot $dot1 = null, LayerDownDot $dot2 = null )
+	{
+		if( is_null( $dot1 ) && is_null( $dot2 ) )
+		{
+			$result = null;
+		}
+		else
+		{
+			if( ( ! is_null( $dot1 ) ) && ( ! is_null( $dot2 ) ) )
+			{
+				$result = array( "dot1" => $dot1, "dot2" => $dot2 );
+			}
+			else
+			{
+				throw new \Exception( 'Incoherent LayerDownDots as straight neighbours.' );
+			}
+		}
+		
+		return $result;
+	}
+	
+	private function getSameDiagonalNeighbour( LayerDownDot $dot1 = null, LayerDownDot $dot2 = null )
+	{
+		if( $dot1 !== $dot2 )
+		{
+			throw new \Exception( 'Not same LayerDownDots as diagonal neighbours.' );
+		}
+		
+		return $dot1;
 	}
 }
--- tests/LayerDownDotTest.php
+++ tests/LayerDownDotTest.php
@@ -1,12 +1,19 @@
 <?php
 
+use Xmontero\EpigraphInvalidation\LayerDownDot;
+
 class LayerDownDotTest extends PHPUnit_Framework_TestCase
 {
 	private $sut = null;
 	
 	public function setUp()
 	{
-		$this->sut = new Xmontero\EpigraphInvalidation\LayerDownDot;
+		$this->sut = new LayerDownDot;
+	}
+	
+	public function tearDown()
+	{
+		unset( $this->sut );
 	}
 	
 	public function testFace()
@@ -20,8 +27,91 @@ class LayerDownDotTest extends PHPUnit_Framework_TestCase
 		$this->assertTrue( $this->sut->getFilled() );
 	}
 	
-	public function tearDown()
+	public function testNoNeighboursAddedReturnsNull()
 	{
-		unset( $this->sut );
+		// +---+
+		// |sut|
+		// +---+
+		
+		$this->assertNull( $this->sut->getLeftNeighbour() );
+		$this->assertNull( $this->sut->getRightNeighbour() );
+		$this->assertNull( $this->sut->getTopNeighbour() );
+		$this->assertNull( $this->sut->getBottomNeighbour() );
+	}
+	
+	public function testSimpleNeighboursAddedReturnObjectsOnEdgesAndNullOnDiagonals()
+	{
+		//     +---+
+		//     | T |
+		// +---+---+---+
+		// | L |sut| R |
+		// +---+---+---+
+		//     | B |
+		//     +---+
+		
+		$dotLeft = new LayerDownDot;
+		$dotRight = new LayerDownDot;
+		$dotTop = new LayerDownDot;
+		$dotBottom = new LayerDownDot;
+		
+		$this->sut->setLeftNeighbour( $dotLeft );
+		$this->sut->setRightNeighbour( $dotRight );
+		$this->sut->setTopNeighbour( $dotTop );
+		$this->sut->setBottomNeighbour( $dotBottom );
+		
+		$this->assertSame( $dotLeft, $this->sut->getLeftNeighbour() );
+		$this->assertSame( $dotRight, $this->sut->getRightNeighbour() );
+		$this->assertSame( $dotTop, $this->sut->getTopNeighbour() );
+		$this->assertSame( $dotBottom, $this->sut->getBottomNeighbour() );
+		
+		$this->assertNull( $this->sut->getTopLeftNeighbour() );
+		$this->assertNull( $this->sut->getTopRightNeighbour() );
+		$this->assertNull( $this->sut->getBottomLeftNeighbour() );
+		$this->assertNull( $this->sut->getBottomRightNeighbour() );
+	}
+	
+	public function testFullNeighboursAddedReturnObjectsOnEdgesAndDiagonals()
+	{
+		// +---+---+---+
+		// |TL | T |TR |
+		// +---+---+---+
+		// | L |sut| R |
+		// +---+---+---+
+		// |BL | B |BR |
+		// +---+---+---+
+		
+		$dotLeft = new LayerDownDot;
+		$dotRight = new LayerDownDot;
+		$dotTop = new LayerDownDot;
+		$dotBottom = new LayerDownDot;
+		
+		$dotTopLeft = new LayerDownDot;
+		$dotTopRight = new LayerDownDot;
+		$dotBottomLeft = new LayerDownDot;
+		$dotBottomRight = new LayerDownDot;
+		
+		$dotLeft->setTopNeighbour( $dotTopLeft );
+		$dotLeft->setBottomNeighbour( $dotBottomLeft );
+		$dotRight->setTopNeighbour( $dotTopRight );
+		$dotRight->setBottomNeighbour( $dotBottomRight );
+		$dotTop->setLeftNeighbour( $dotTopLeft );
+		$dotTop->setRightNeighbour( $dotTopRight );
+		$dotBottom->setLeftNeighbour( $dotBottomLeft );
+		$dotBottom->setRightNeighbour( $dotBottomRight );
+		
+		$this->sut->setLeftNeighbour( $dotLeft );
+		$this->sut->setRightNeighbour( $dotRight );
+		$this->sut->setTopNeighbour( $dotTop );
+		$this->sut->setBottomNeighbour( $dotBottom );
+		
+		$this->assertSame( $dotLeft, $this->sut->getLeftNeighbour() );
+		$this->assertSame( $dotRight, $this->sut->getRightNeighbour() );
+		$this->assertSame( $dotTop, $this->sut->getTopNeighbour() );
+		$this->assertSame( $dotBottom, $this->sut->getBottomNeighbour() );
+		
+		$this->assertSame( $dotTopLeft, $this->sut->getTopLeftNeighbour() );
+		$this->assertSame( $dotTopRight, $this->sut->getTopRightNeighbour() );
+		$this->assertSame( $dotBottomLeft, $this->sut->getBottomLeftNeighbour() );
+		$this->assertSame( $dotBottomRight, $this->sut->getBottomRightNeighbour() );
 	}
 }
